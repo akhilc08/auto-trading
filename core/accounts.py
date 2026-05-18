705b8f81847a6125f512c49a787a6b488b135671
akhilc08 <akhilchilakawork@gmail.com>
2026-05-17 22:08:13 -0400

    Consolidate to 3 accounts: merge market_neutral into stat_arb

--- core/accounts.py
+++ core/accounts.py
@@ -1,8 +1,7 @@
 _ACCOUNT_STRATEGIES: dict[str, list[str]] = {
-    "stat_arb":       ["stat_arb", "stat_arb_v2", "stat_arb_v3"],
-    "macro_vol":      ["trend_following", "trend_following_v2", "regime_switching", "vol_risk_premium"],
-    "market_neutral": ["market_neutral", "market_neutral_v2"],
-    "stock_alpha":    ["multi_factor_equity", "multi_factor_equity_v2", "post_earnings_drift"],
+    "stat_arb":   ["stat_arb", "stat_arb_v2", "stat_arb_v3", "market_neutral", "market_neutral_v2"],
+    "macro_vol":  ["trend_following", "trend_following_v2", "regime_switching", "vol_risk_premium"],
+    "stock_alpha": ["multi_factor_equity", "multi_factor_equity_v2", "post_earnings_drift"],
 }
 
 _STRATEGY_ACCOUNT: dict[str, str] = {
